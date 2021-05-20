6399f46c9b1222565dd2e731efbaca2af7297b93
Carlos Castillo <castillo25ca@gmail.com>
2021-05-20 15:17:38 -0700

    update to style.css

--- src/components/table.js
+++ src/components/table.js
@@ -1,29 +1,48 @@
 import React from 'react'
 
-function Table(props){
+function Table(props) {
 
     return (
-        <table style={{width: "100%"}}>
+        <table style={{ width: "100%" }}>
             <thead>
                 <tr>
-                <th>Image</th>
-                <th>Name</th>
-                <th>Phone Number <button className='filter' onClick={props.sortNumber}><i className="fas fa-filter"></i></button></th>
-                <th>Email <button className='filter' onClick={props.sortEmail}><i className="fas fa-filter"></i></button></th>
-                <th>Location</th>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Phone Number <button className='filter' onClick={props.sortNumber}><i className="fas fa-filter"></i></button>
+                    </th>
+                    <th>
+                        Email <button className='filter' onClick={props.sortEmail}><i className="fas fa-filter"></i></button>
+                    </th>
+                    <th>
+                        Location
+                    </th>
+                    <th>
+                        Image
+                    </th>
                 </tr>
             </thead>
             <tbody>
-                {props.list.map(user => 
-                <tr style={{borderTop: "2px solid gray"}}>
-                <td>
-                    <img src={user.picture.thumbnail} alt="profile"/>
-                </td>
-                <td>{user.name.first} {user.name.last}</td>
-                <td>{user.phone}</td>
-                <td>{user.email}</td>
-                <td>{user.location.state}, {user.location.country}</td>
-                </tr>
+                {props.list.map(user =>
+                    <tr style={{ borderTop: "2px solid gray" }}>
+
+                        <td>
+                            {user.name.first} {user.name.last}
+                        </td>
+                        <td>
+                            {user.phone}
+                        </td>
+                        <td>
+                            {user.email}
+                        </td>
+                        <td>
+                            {user.location.state}, {user.location.country}
+                        </td>
+                        <td>
+                            <img src={user.picture.thumbnail} alt="profile" />
+                        </td>
+                    </tr>
                 )}
             </tbody>
         </table>
